32d1c7d02da188c66fe8f866c33b0f813f30aaf1
Likutar <matheusvizu@gmail.com>
2017-12-03 12:45:45 +0000

    Parser.h

--- ArvoreB.h
+++ ArvoreB.h
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+struct Key{
+    char chave[6];
+    long posicao;
+};
 struct Btree{
     struct Btree* ponteiros;
-    char **chaves;
+    struct Key *chaves;
     int QuantidadeDeChaves;
     char Filho;
 };
 
 struct Btree* CriarArvore(int tamanho){
-    int i;
     struct Btree *arvore;
     arvore = (struct Btree*)malloc(sizeof(arvore));
     arvore->QuantidadeDeChaves = tamanho;
     arvore->ponteiros = (struct Btree*)calloc(tamanho+1, (sizeof &arvore));
-    arvore->chaves = (char**) malloc(sizeof(char*)*tamanho);
-    for(i=0;i<tamanho;i++){
-        arvore->chaves[i] = (char*) malloc(sizeof(char)*7);
-    }
+    arvore->chaves = (struct Key*) calloc(tamanho, sizeof(struct Key));
     arvore->Filho=1;
     return arvore;
+}
+int CalcularTamanho(int pos){
+    int tamanho;
+    
+    return tamanho;
+}
+int ProjetarArvore(struct Key* chaveiro, struct Key* chaveprim){
+    int tamanho;
+    return tamanho;
+}
+void AdicionarChave(struct Btree* raiz,struct Key *chave){
+    int i;
 }
--- Trabalho2.c
+++ Trabalho2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "ArvoreB.h"
+#include "Parser.h"
 
 int main (){
     struct Btree *arvore= NULL;
-    arvore = CriarArvore(2);
+    FILE *DB;
+    Parse(DB, arvore);
     printf("tamanho da arvore= %d\n", arvore->QuantidadeDeChaves);
     return 0;
 }
